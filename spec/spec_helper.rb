8b355436fbfb43bb33c1d877fb61bb38610938ac
Michael Evans <michaeljevans113@gmail.com>
2020-09-19 17:36:01 -0600

    Remove SimpleCov filters for directories that no longer exist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/spec/'
-  add_filter '/app/jobs/'
-  add_filter '/app/views/'
-  add_filter '/app/mailers/'
-  add_filter '/app/channels/'
 end
 require 'rails_helper'
 require 'webmock/rspec'
