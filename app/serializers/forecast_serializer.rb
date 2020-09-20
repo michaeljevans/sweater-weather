0d0c5eafc2e28584e7bfff016a574f3376db89ac
Michael Evans <michaeljevans113@gmail.com>
2020-09-20 11:52:01 -0600

    Create ForecastSerializer to process data from the ForecastFacade

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,5 +1,12 @@
 class ForecastSerializer
   def self.serialize_me_captain(forecast)
-
+    {
+      location: forecast.location.as_json,
+      weather: {
+        current: forecast.current_weather.as_json,
+        hourly: forecast.hourly_weather.map { |hour| hour.as_json },
+        daily: forecast.daily_weather.map { |day| day.as_json }
+      }
+    }
   end
 end
