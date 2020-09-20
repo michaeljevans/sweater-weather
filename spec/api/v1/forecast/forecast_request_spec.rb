9cf27f3d929a1b863832f9fd9dc68be5e0e707b2
Michael Evans <michaeljevans113@gmail.com>
2020-09-19 21:19:01 -0600

    Create spec for location-based forecast request

--- spec/api/v1/forecast/forecast_request_spec.rb
+++ spec/api/v1/forecast/forecast_request_spec.rb
@@ -1,5 +1,16 @@
 RSpec.describe 'Forecast API' do
   scenario 'returns current, hourly, and daily weather info for a given city' do
+    VCR.use_cassette('forecast_denver_co', re_record_interval: 7.days) do
+      location = 'denver,co'
 
+      get "/api/v1/forecast?location=#{location}"
+
+      expect(response).to be_successful
+      expect(response.content_type).to eq('application/json')
+
+      forecast = JSON.parse(response.body, symbolize_names: true)
+
+      expect(forecast.class).to eq(Hash)
+    end
   end
 end
