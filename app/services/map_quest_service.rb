9c8b2dbd7decc8cc2242075e916950bf7cee405e
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 11:13:12 -0600

    Add .get_distance_between()

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -15,4 +15,22 @@ class MapQuestService
 
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_distance_between(location_1, location_2)
+    domain = 'http://www.mapquestapi.com'
+    uri    = '/directions/v2/route'
+
+    params = {
+      key: ENV['MAPQUEST_API_KEY'],
+      from: location_1,
+      to: location_2
+    }
+
+    response = Faraday.get("#{domain}#{uri}") do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params                  = params
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
