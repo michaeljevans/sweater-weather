0ec9e0ff0dbf5ad4574165d223bd16849f6b6398
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 09:35:09 -0600

    Add route for climbing_routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
+
+      get '/climbing_routes', to: 'climbing#index'
     end
   end
 end
