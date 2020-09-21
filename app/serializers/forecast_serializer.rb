bfc3f3b8c1e182d4453c5e1468b7aec861746167
Michael Evans <michaeljevans113@gmail.com>
2020-09-20 23:50:41 -0600

    Adjust structure of JSON response

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,11 +1,13 @@
 class ForecastSerializer
   def self.serialize_me_captain(forecast)
     {
-      location: forecast.location.as_json,
-      weather: {
-        current: forecast.current_weather.as_json,
-        hourly: forecast.hourly_weather.map { |hour| hour.as_json },
-        daily: forecast.daily_weather.map { |day| day.as_json }
+      forecast: {
+        location: forecast.location.as_json,
+        weather: {
+          current: forecast.current_weather.as_json,
+          hourly: forecast.hourly_weather.map { |hour| hour.as_json },
+          daily: forecast.daily_weather.map { |day| day.as_json }
+        }
       }
     }
   end
