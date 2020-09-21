eda4527aa7585642a6b4c6b33cd7bcd9439ba2f6
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 11:47:31 -0600

    Change serializer call to .new for FastJSON instead of class method for handrolled

--- app/controllers/api/v1/climbing_controller.rb
+++ app/controllers/api/v1/climbing_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ClimbingController < ApplicationController
   def index
     climbing_routes = ClimbingRoutesFacade.new(params[:location])
-    render json: ClimbingRoutesSerializer.cerealize_me_captain(climbing_routes)
+    render json: ClimbingRoutesSerializer.new(climbing_routes)
   end
 end
