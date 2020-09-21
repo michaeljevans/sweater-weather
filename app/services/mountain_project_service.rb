04e8d9cbcd7a240fd2bdaaec9eb91938e80d5d2f
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 10:09:13 -0600

    Create MountainProject service

---
+++ app/services/mountain_project_service.rb
@@ -0,0 +1,20 @@
+class MountainProjectService
+  def self.get_nearby_routes(latitude, longitude)
+    domain = 'https://www.mountainproject.com'
+    uri    = '/data/get-routes-for-lat-lon'
+
+    params = {
+      key: ENV['MOUNTAIN_PROJECT_API_KEY'],
+      lat: latitude,
+      lon: longitude,
+      maxDistance: 50
+    }
+
+    response = Faraday.get("#{domain}#{uri}") do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params                  = params
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
