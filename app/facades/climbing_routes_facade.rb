4ffad50f976dc869133f733107f7151934c1d0ff
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 09:44:44 -0600

    Aggregating information I need

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -1,2 +1,29 @@
 class ClimbingRoutesFacade
+  attr_reader :location
+  def initialize(location)
+    @location = location_info(location)
+    @forecast = climbing_forecast
+    @routes   = nearby_climbing_routes
+  end
+
+  def location_info(location)
+    response = MapQuestService.get_location_information(location)
+    info = response[:results].first[:locations].first
+    Location.new(info)
+  end
+
+  def climbing_forecast
+    current_weather = forecast[:current]
+    ClimbingForecast.new(current_weather)
+  end
+
+  def nearby_climbing_routes
+    
+  end
+
+  private
+
+  def forecast
+    @forecast ||= OpenWeatherService.get_forecast_information(@location.latitude, @location.longitude)
+  end
 end
