1358b4d31c7da8f4ca857d04823092f2861a4892
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 20:46:55 -0600

    Create Unsplash service

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,19 @@
+class UnsplashService
+  def self.images_by_search(terms)
+    domain = 'https://api.unsplash.com'
+    uri    = '/search/photos'
+
+    params = {
+      client_id: ENV['UNSPLASH_ACCESS_KEY'],
+      query: "#{terms},main,street"
+    }
+
+    response = Faraday.get("#{domain}#{uri}") do |req|
+      req.headers['Content-Type']   = 'application/json'
+      req.headers['Accept-Version'] = 'v1'
+      req.params                    = params
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
