de7a432bee0ac40daf5ffe83c9fb82d034486a89
Michael Evans <michaeljevans113@gmail.com>
2020-09-21 20:59:28 -0600

    Create spec for ImageFacade

---
+++ spec/facades/image_facade_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe ImageFacade do
+  it 'exists and has attributes' do
+    VCR.use_cassette('unsplash_denver') do
+      search_term = 'denver'
+
+      image = ImageFacade.new(search_term)
+
+      expect(image).to be_an(ImageFacade)
+      expect(image.location).to eq('denver')
+      expect(image.image_url).to be_a(String)
+      expect(image.credit).to be_an(ImageCredit)
+    end
+  end
+end
