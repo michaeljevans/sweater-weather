e58afdb2a63426a7732e90d939d72a792f4689c7
Michael Evans <michaeljevans113@gmail.com>
2020-09-22 12:08:47 -0600

    Happy path for user login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,9 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(params[:email])
-    render json: UserSerializer.serialize_me_captain(user)
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.serialize_me_captain(user), status: 201
+    end
   end
 end
--- spec/api/v1/sessions/user_login_request_spec.rb
+++ spec/api/v1/sessions/user_login_request_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'User login API' do
   it 'allows a user to log in with valid credentials' do
-    User.create!(email: 'whatever@example.com', password: 'correctpassword')
+    user = User.create!(email: 'whatever@example.com', password: 'correctpassword')
 
     headers = {
       'Content-Type': 'application/json',
@@ -15,6 +15,21 @@ RSpec.describe 'User login API' do
     post '/api/v1/sessions', headers: headers, params: params.to_json
 
     expect(response).to be_successful
+    expect(response.status).to eq(201)
     expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to be_a(Hash)
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data]).to have_key(:type)
+    expect(parsed[:data][:type]).to eq('users')
+    expect(parsed[:data]).to have_key(:id)
+    expect(parsed[:data][:id]).to eq(user.id)
+    expect(parsed[:data]).to have_key(:attributes)
+    expect(parsed[:data][:attributes]).to have_key(:email)
+    expect(parsed[:data][:attributes][:email]).to eq(user.email)
+    expect(parsed[:data][:attributes]).to have_key(:api_key)
+    expect(parsed[:data][:attributes][:api_key]).to eq(user.api_key)
   end
 end
