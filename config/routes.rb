c6e4027b18ce12ab4f83581dffe0f198ca2b762f
Michael Evans <michaeljevans113@gmail.com>
2020-09-22 00:00:46 -0600

    Add route for user creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#show'
 
       get '/backgrounds', to: 'background_image#index'
+
+      post '/users', to: 'users#create'
     end
   end
 end
