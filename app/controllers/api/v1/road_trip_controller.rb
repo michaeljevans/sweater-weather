e0fe578c4f981bd727b220b2be9acdb86c9e14ed
Michael Evans <michaeljevans113@gmail.com>
2020-09-22 17:13:54 -0600

    Branching to test something

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    user = User.find_by(api_key: road_trip_params[:api_key])
-    if user
+    if @user
       
+      render json: RoadTripSerializer.serialize_me_captain(road_trip)
+    end
   end
 
   private
 
   def road_trip_params
     params.permit(:origin, :destination, :api_key)
+  end
+
+  def api_key_valid?
+    @user ||= User.find_by(api_key: road_trips_params[:api_key])
   end
 end
