ffdee55de65461798a5f58c44f15468a85d621ce
Michael Evans <michaeljevans113@gmail.com>
2020-09-23 00:33:49 -0600

    Add sad path tests

--- spec/api/v1/road_trip/road_trip_creation_request_spec.rb
+++ spec/api/v1/road_trip/road_trip_creation_request_spec.rb
@@ -41,4 +41,183 @@ RSpec.describe 'Road Trip API' do
       expect(parsed[:data][:attributes][:arrival_forecast][:description]).to be_a(String)
     end
   end
+
+  it 'cannot create a road trip without an origin' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      destination: 'las vegas, nv',
+      api_key: user.api_key
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Origin, destination, and API key are required fields.')
+  end
+
+  it 'cannot create a road trip without a destination' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      origin: 'denver, co',
+      api_key: user.api_key
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Origin, destination, and API key are required fields.')
+  end
+
+  it 'cannot create a road trip without an API key' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      origin: 'denver, co',
+      destination: 'las vegas, nv'
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Origin, destination, and API key are required fields.')
+  end
+
+  it 'cannot create a road trip with an empty origin' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      origin: '',
+      destination: 'las vegas, nv',
+      api_key: user.api_key
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Origin, destination, and API key are required fields.')
+  end
+
+  it 'cannot create a road trip with an empty destination' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      origin: 'denver, co',
+      destination: '',
+      api_key: user.api_key
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Origin, destination, and API key are required fields.')
+  end
+
+  it 'cannot create a road trip with an empty API key' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      origin: 'denver, co',
+      destination: 'las vegas, nv',
+      api_key: ''
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Origin, destination, and API key are required fields.')
+  end
+
+  it 'cannot create a road trip with an invalid API key' do
+    user = User.create!(email: 'whatever@example.com', password: 'password')
+
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    params = {
+      origin: 'denver, co',
+      destination: 'las vegas, nv',
+      api_key: 'not a real api key'
+    }
+
+    post '/api/v1/road_trip', headers: headers, params: params.to_json
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.content_type).to eq('application/json')
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq('Invalid API key.')
+  end
 end
