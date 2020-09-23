8efea327de7b92bf975d4292735b72551a8765cf
Michael Evans <michaeljevans113@gmail.com>
2020-09-22 20:53:41 -0600

    Happy path for RoadTrip creation

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,18 +1,19 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    if @user
-      
-      render json: RoadTripSerializer.serialize_me_captain(road_trip)
+    if api_key_valid?
+      road_trip = @user.road_trips.create(road_trip_params)
+      facade = RoadTripFacade.new(road_trip.id, road_trip_params)
+      render json: RoadTripSerializer.serialize_me_captain(facade), status: 201
     end
   end
 
   private
 
   def road_trip_params
-    params.permit(:origin, :destination, :api_key)
+    params.permit(:origin, :destination)
   end
 
   def api_key_valid?
-    @user ||= User.find_by(api_key: road_trips_params[:api_key])
+    @user ||= User.find_by(api_key: params[:api_key])
   end
 end
