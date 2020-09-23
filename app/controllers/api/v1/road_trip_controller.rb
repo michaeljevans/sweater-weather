06074893836983a13a34398bb9d522dd5450be4c
Michael Evans <michaeljevans113@gmail.com>
2020-09-23 00:36:40 -0600

    Add sad paths

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    if api_key_valid?
+    if any_params_empty?
+      render json: { error: 'Origin, destination, and API key are required fields.' }, status: 400
+    elsif !api_key_valid?
+      render json: { error: 'Invalid API key.' }, status: 401
+    else
       road_trip = @user.road_trips.create(road_trip_params)
-      facade = RoadTripFacade.new(road_trip.id, road_trip_params)
+      facade = RoadTripFacade.new(road_trip)
       render json: RoadTripSerializer.serialize_me_captain(facade), status: 201
     end
   end
@@ -11,6 +15,11 @@ class Api::V1::RoadTripController < ApplicationController
 
   def road_trip_params
     params.permit(:origin, :destination)
+  end
+
+  def any_params_empty?
+    input = [ params[:origin], params[:destination], params[:api_key] ]
+    input.any? { |param| param.nil? || param.empty? }
   end
 
   def api_key_valid?
