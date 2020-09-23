3cbe2bc303a848cc0ed7c6526348b8a70f2eb429
Michael Evans <michaeljevans113@gmail.com>
2020-09-22 23:32:23 -0600

    Add strong params

--- app/controllers/api/v1/background_image_controller.rb
+++ app/controllers/api/v1/background_image_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::BackgroundImageController < ApplicationController
   def index
-    background_image = ImageFacade.new(params[:location])
+    background_image = ImageFacade.new(image_params[:location])
     render json: ImageSerializer.serialize_me_captain(background_image)
+  end
+
+  private
+
+  def image_params
+    params.permit(:location)
   end
 end
